a72e31c405296abd4b514446a8fa2f7e0363b682
ishizawa <garageshop99@yahoo.co.jp>
2014-02-20 12:03:06 +0900

    add projection mapping mode, start/stop, background, fullscreen, yencount reset, small to 10yen

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -5,10 +5,21 @@
 void testApp::setup(){
     
     //背景
+    ofSetBackgroundAuto(true);
     ofBackground(0, 0, 0);
+    ofSetFrameRate(60);
+    //ofSetFullscreen(true);
+
+    int w = 1300;
+    int h = ofGetHeight() - 40;
+    int w2 = 240;
+    int h2 = 60;
     
     //画像の読み込み
-    myImage.loadImage("10yen.png");
+    myImage.loadImage("10yen.png"); // お金
+    bgImage.loadImage("bg.png"); // 背景
+    bgYenImage.loadImage("yen_frame.png"); // 金額の背景
+    manualImage.loadImage("manual.png"); // マニュアル
     
     //指定したポートで接続
     receiver.setup( PORT );
@@ -20,27 +31,57 @@ void testApp::setup(){
     //Box2Dの基本設定
     box2d.init(); //初期化
     box2d.registerGrabbing(); //物体をつかめるように設定
-    box2d.setGravity(0, 1); //重力の設定(下方向に1)
-    box2d.createBounds(0, 0, ofGetWidth(), ofGetHeight()); //枠を追加
-    box2d.setFPS(15.0); //Box2Dの演算の更新頻度を設定
+    box2d.setGravity(0, 1.2); // x, y方向の重力を設定(下方向に1)
+    box2d.createBounds(0, 0, 1390, ofGetHeight()); //枠を追加
+    box2d.setFPS(15); //Box2Dの演算の更新頻度を設定
     box2d.setIterations(5, 5); //インタラクションの精度を設定
     
     // 音声
     mySound.loadSound("yen.mp3"); //サウンドファイルの読込み
     sound1up.loadSound("1up.wav"); //サウンドファイルの読込み
     levelup.loadSound("levelup.wav"); //サウンドファイルの読込み
-    mySound.setLoop(false); //ループ再生をONに
-    sound1up.setLoop(false); //ループ再生をONに
-    levelup.setLoop(false); //ループ再生をONに
+    mySound.setLoop(false); //ループ再生をOFFに
+    sound1up.setLoop(false); //ループ再生をOFFに
+    levelup.setLoop(false); //ループ再生をOFFに
     
     // テキスト
-    font.loadFont("verdana.ttf", 20);
-    yen.loadFont("verdana.ttf", 15);
+    font.loadFont("tare.ttf", 40);
+    yen.loadFont("tare.ttf", 15, true, true);
+    yenString = "円";
+    //manual.loadFont("tare.ttf", 15, true, true);
+    //manualString = "[p] -- プロジェクションマッピングモード<br>てすと";
+    
+    p1 = ofPoint(100, 200);
+    p2 = ofPoint(200, 300);
+    p3 = ofPoint(300, 400);
+    
+    
     num = 0;
     
+    // BOX（実際の表示）
+    fbo.allocate( 1390, ofGetHeight());
+    
+    // 金額（実際の表示）
+    fbo2.allocate( w2, h2 );
+    
+    warper.setSourceRect( ofRectangle( 0, 0, w, h ) );
+    warper.setTopLeftCornerPosition( ofPoint( x, y ) );
+    warper.setTopRightCornerPosition( ofPoint( x + w, y ) );
+    warper.setBottomLeftCornerPosition( ofPoint( x, y + h ) );
+    warper.setBottomRightCornerPosition( ofPoint( x + w, y + h ) );
+    warper.setup();
+    
+    warper2.setSourceRect( ofRectangle( 0, 0, w2, h2 ) );
+    warper2.setTopLeftCornerPosition( ofPoint( x2, y2 ) );
+    warper2.setTopRightCornerPosition( ofPoint( x2 + w2, y2 ) );
+    warper2.setBottomLeftCornerPosition( ofPoint( x2, y2 + h2 ) );
+    warper2.setBottomRightCornerPosition( ofPoint( x2 + w2, y2 + h2 ) );
+    warper2.setup();
     
     
-    
+    // ラインを消す
+    warper.toggleShow();
+    warper2.toggleShow();
 }
 
 //--------------------------------------------------------------
@@ -52,26 +93,24 @@ void testApp::update(){
     // 現在順番待ちのOSCメッセージがある間は受信を続ける
     while(receiver.hasWaitingMessages()){
         
+        //num = num+10;
+        
         //次のメッセージを取得
         ofxOscMessage m;
         receiver.getNextMessage(&m);
-        
+
+        //dumpOSC(m);
+
         // メッセージが /mouse/position ならマウスの位置を取得
         if (m.getAddress() == "/mouse/position"){
             remoteMouse.x = m.getArgAsInt32(0);
             remoteMouse.y = m.getArgAsInt32(1);
-            
         }
+        
         // メッセージが /mouse/button ならマウスボタンの状態を取得
         else if (m.getAddress() == "/mouse/button"){
             mouseButtonState = m.getArgAsString(0) ;
         }
-        
-        //OSCメッセージをそのままコンソールに出力
-        //dumpOSC(m);
-        printf("test");
-        
-        
     }
 }
 
@@ -80,34 +119,24 @@ void testApp::draw(){
     
     time++;
     
-    //ofSetColor(31, 63, 255); //描画色の設定
-    //ofCircle(mouseX, mouseY, 40); //マウスの現在位置を中心に円を描く
-    //circlesに格納された全ての円を描画(イテレータ使用)
-    for(vector <ofxBox2dCircle *>::iterator it = circles.begin(); it != circles.end(); ++it) {
+    // wrapする
+    fbo.begin();
+    {
+        bgImage.draw(0,0);
+        bgImage.draw(1000,0);
+        bgImage.draw(1000,1000);
+        bgImage.draw(1000,1000);
+        manualImage.draw(ofGetWidth() * 0.25, ofGetHeight() * 0.25);
+
+        //circlesに格納された全ての円を描画(イテレータ使用)
+        for(vector <ofxBox2dCircle *>::iterator it = circles.begin(); it != circles.end(); ++it) {
         (*it)->draw();
+        }
+
     }
-    
-    //Box2Dで生成された図形を描画
-    box2d.draw();
-    
-    //描画した円(ofxBox2dCircle)の数だけ、円を描画する
-    /*
-     for(int i=0; i<circles.size(); i++) {
-     circles[i].draw();
-     }
-     */
-    
-    
-    
+    fbo.end();
     // マウスボタンが押されていたら
-    if (mouseButtonState == "down") {
-        
-        //time2 = time;
-        
-        //if(mouseButtonState == "up"){
-        
-        //font.drawString(ofToString(ofGetMinutes()), 400, 50);
-        
+    if (mouseButtonState == "down" && nage == true) {
         
         //ofxBox2dCircle(円)クラスをインスタンス化
         ofxBox2dCircle* circle = new ofxBox2dCircle();
@@ -118,9 +147,12 @@ void testApp::draw(){
         //半径を設定
         float r = ofRandom(10, 30);
         //物理パラメータを設定(重さ、反発力、摩擦力)
-        circle->setPhysics(1.0, 0.8, 0.5);
+        circle->setPhysics(3.0, 0.8, 0.8);
+        
         //マウスの位置に円を設定
-        circle->setup(box2d.getWorld(), pos.x, pos.y, r);
+        //circle->setup(box2d.getWorld(), pos.x, pos.y, r);
+        circle->setup(box2d.getWorld(), ofRandom(0, 1390), pos.y, r);
+        
         //生成した円をcirclesに追加
         circles.push_back(circle);
         
@@ -141,34 +173,51 @@ void testApp::draw(){
             levelup.play();
         }
         
-        
-        
-        //画面をタッチすると新たに円(ofxBox2dCircle)を追加する
-        /*
-         ofxBox2dCircle c; //新規にofxBox2dCircleをインスタンス化
-         c.setPhysics(1.0, 0.8, 0.3); //物理特性を設定
-         */
-        //Box2Dの世界に追加
-        /*
-         c.setup(box2d.getWorld(),
-         pos.x,
-         pos.y,
-         ofRandom(10, 30));
-         circles.push_back(c); //Vector、circlesに作成した円を追加
-         mySound.play(); //サウンド再生開始
-         num = num+10; // コイン枚数カウント
-         */
-        //}
-        
     }
     
-    // テキスト
-    ofSetColor(255, 255, 255);
-    font.drawString(ofToString(num), 50, 50);
-    font.drawString("yen", 140, 50);
-    //font.drawString(ofToString(time), 400, 50);
+        //======================== get our quad warp matrix.
+        
+        ofMatrix4x4 mat = warper.getMatrix();
+        ofMatrix4x4 mat2 = warper2.getMatrix();
+
+        //======================== use the matrix to transform our fbo.
+        
+        glPushMatrix();
+        glMultMatrixf( mat.getPtr() );
+        {
+            fbo.draw( 0, 0 );
+        }
+        glPopMatrix();
+
     
+        glPushMatrix();
+        glMultMatrixf( mat2.getPtr() );
+        {
+            fbo2.draw( 0, 0 );
+        }
+        glPopMatrix();
     
+        //======================== use the matrix to transform points.
+        
+
+        
+    // wrapする
+    fbo2.begin();
+    {
+        //ofBackground(0, 0, 0, 0.6);
+        bgYenImage.draw(0,0);
+        
+        // テキスト
+        ofSetColor(60, 30, 5);
+        yen.drawString(yenString, 160, 50); // 円
+        font.drawString(ofToString(num), 30, 50); // 数字
+    }
+    fbo2.end();
+
+    // wrap描画
+    warper2.draw();
+    warper.draw();
+
 }
 
 //--------------------------------------------------------------
@@ -178,7 +227,37 @@ void testApp::exit(){
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-    if (key == 'z'){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+    
+    // プロジェクションマッピング用のライン
+    if( key == 'p' ){
+        warper.toggleShow();
+        warper2.toggleShow();
+    }
+    // 画面フルスクリーン
+    if( key == 'f' ){
+        ofToggleFullscreen();
+    }
+
+    // 投げ受付
+    if( key == 'n' ){
+        if(nage == false){
+            nage = true;
+            manualImage.setAnchorPercent(5, 5);
+
+        } else {
+            nage = false;
+            manualImage.setAnchorPercent(0.05, 0.05);
+
+        }
+    }
+    
+    // 見えているコインだけリセット
+    if (key == 'x'){
         
         //circles配列の全ての要素を消去する(イテレータ使用)
         for(vector <ofxBox2dCircle *>::iterator it = circles.begin(); it != circles.end();){
@@ -187,11 +266,18 @@ void testApp::keyPressed(int key){
             it = circles.erase(it);
         }
     }
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
     
+    // 得点もリセット
+    if (key == 'z'){
+        //circles配列の全ての要素を消去する(イテレータ使用)
+        for(vector <ofxBox2dCircle *>::iterator it = circles.begin(); it != circles.end();){
+            (*it)->destroyShape();
+            delete *it;
+            it = circles.erase(it);
+        }
+        // 得点を0に
+        num = 0;
+    }
 }
 
 //--------------------------------------------------------------
@@ -211,28 +297,7 @@ void testApp::mousePressed(int x, int y, int button){
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
     
-    /*
-     //画面をタッチすると新たに円(ofxBox2dCircle)を追加する
-     ofPoint pos; //タッチした座標を格納するための変数
-     pos.set(mouseX, mouseY); //タッチした場所を取得
-     ofxBox2dCircle c; //新規にofxBox2dCircleをインスタンス化
-     c.setPhysics(1.0, 0.8, 0.3); //物理特性を設定
-     //Box2Dの世界に追加
-     c.setup(box2d.getWorld(),
-     pos.x,
-     10,
-     ofRandom(10, 30));
-     circles.push_back(c); //Vector、circlesに作成した円を追加
-     
-     // 100個以上になったら先頭のofxBox2dCircleを削除
-     if(circles.size() > 100){
-     //vector <ofxBox2dCircle *>::iterator const it = circles.begin();
-     
-     //(*it)->destroy();
-     //delete *it;
-     //it = circles.erase(it);
-     }
-     */
+    if(nage == true) {
     
     //ofxBox2dCircle(円)クラスをインスタンス化
     ofxBox2dCircle* circle = new ofxBox2dCircle();
@@ -262,9 +327,7 @@ void testApp::mouseReleased(int x, int y, int button){
         levelup.play();
     }
     
-    
-    
-    
+    }
 }
 
 //--------------------------------------------------------------
@@ -278,6 +341,6 @@ void testApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
+void testApp::dragEvent(ofDragInfo dragInfo){
     
 }
--- src/testApp.h
+++ src/testApp.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxTrueTypeFontUC.h"
 #include "ofxBox2d.h"
 #include "ofxOsc.h"
+#include "ofxQuadWarp.h"
+
 
 // OSCを受信するポート番号を設定
-#define PORT 8000
+#define PORT 8999
 
 class testApp : public ofBaseApp{
     
@@ -25,15 +28,18 @@ public:
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
     
+    
     void dumpOSC(ofxOscMessage m); //OSCメッセージを出力
     ofxBox2d box2d; //Box2Dのインスタンス
     vector <ofxBox2dCircle *> circles; //円形の物体のインスタンス
     //vector <ofxBox2dCircle>::iterator it;
     
     
-    
     // 画像
-    ofImage myImage;
+    ofImage myImage; // お金の画像
+    ofImage bgImage; // 背景画像
+    ofImage bgYenImage; // 金額の背景画像
+    ofImage manualImage; // マニュアル画像
     
     // OSCメッセージを受信するインスタンス
     ofxOscReceiver receiver;
@@ -49,16 +55,43 @@ public:
     ofSoundPlayer sound1up; //ofSoundクラスをインスタンス化
     ofSoundPlayer levelup; //ofSoundクラスをインスタンス化
     
-    // カウント
+    // お金カウント
     int num;
     
     // 文字
     ofTrueTypeFont font;
-    ofTrueTypeFont yen;
+    //ofTrueTypeFont yen;
+
+    // 円
+    ofxTrueTypeFontUC yen;
+    string yenString;
+    ofPoint p1, p2, p3;
+    ofRectangle rect1, rect2, rect3;
+    vector<ofPath> characters;
+
+    // マニュアル
+    ofxTrueTypeFontUC manual;
+    string manualString;
     
     // 秒
     int time;
     int time2;
     int time3;
+  
+    // 投げ受付
+    bool nage = false;
     
+    // プロジェクションマッピング
+    ofxQuadWarp         warper;
+    ofxQuadWarp         warper2;
+    ofFbo               fbo;
+    ofFbo               fbo2;
+    ofPoint             points[ 10 ];
+    ofPoint             points2[ 10 ];
+    
+    // wrap位置
+    int x = 0;
+    int y = 0;
+    int x2 = 20;
+    int y2 = 20;
 };
